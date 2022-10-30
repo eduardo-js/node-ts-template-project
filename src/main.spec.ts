1527fed79379ecbec08d9facd6eeab6cddb07cbd
eduardo-js <jose.eduardo.ds@gmail.com>
2022-10-29 22:26:28 -0300

    chore(dotenv-cli): adding dotenv cli

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -4,4 +4,7 @@ describe('main', () => {
 	it('should be defined', async () => {
 		expect(main).toBeDefined();
 	});
+	it('should 3000', async () => {
+		expect(process.env['PORT']).toEqual('123');
+	});
 });
